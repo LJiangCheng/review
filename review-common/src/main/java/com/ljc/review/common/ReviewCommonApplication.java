dfd9a4f98f0c6f329d9953c1de95275e68ae4b72
lijiangc <lijiangcheng@git.com>
2018-10-19 11:12:17 +0800

    First Commit

---
+++ review-common/src/main/java/com/ljc/review/common/ReviewCommonApplication.java
@@ -0,0 +1,12 @@
+package com.ljc.review.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewCommonApplication.class, args);
+	}
+}
