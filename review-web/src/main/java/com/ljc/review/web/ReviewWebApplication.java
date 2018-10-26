3b216b17e8f62fcb90995336f6fb33b7c2a297f6
lijiangc <lijiangcheng@git.com>
2018-10-26 15:22:35 +0800

    First Commit

---
+++ review-web/src/main/java/com/ljc/review/web/ReviewWebApplication.java
@@ -0,0 +1,12 @@
+package com.ljc.review.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewWebApplication.class, args);
+	}
+}
