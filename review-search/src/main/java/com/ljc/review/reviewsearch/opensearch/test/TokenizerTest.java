f9132dea9281a6c5e108240fec74b75a92bb9515
lijiangc <lijiangcheng@git.com>
2019-01-16 09:35:57 +0800

    commit test

--- review-search/src/main/java/com/ljc/review/reviewsearch/opensearch/test/TokenizerTest.java
+++ review-search/src/main/java/com/ljc/review/reviewsearch/opensearch/test/TokenizerTest.java
@@ -24,11 +24,16 @@ public class TokenizerTest {
     public void testJieBa() throws IOException {
         JiebaSegmenter segmenter = new JiebaSegmenter();
         WordDictionary wd = WordDictionary.getInstance();
-        //加载自定义词典 注：jeiba自定义词典规则：一行一个词，分别为：词条/词频/词性(可省略)，之间用空格分开
-        wd.loadUserDict(Paths.get(ResourceUtils.getFile("classpath:user-dict.txt").getAbsolutePath()));
-        String str = "手电钻 220V";
-        str = str.replaceAll(" ", "");
+        String str = "300~800W、13mm";
+        //str = str.replaceAll(" ", "");
         List<SegToken> process = segmenter.process(str, JiebaSegmenter.SegMode.SEARCH);
+        for (SegToken st : process) {
+            String word = st.word;
+            System.out.println(word);
+        }
+        //加载自定义词典 PS：jeiba自定义词典规则：一行一个词，分别为：词条/词频/词性(可省略)，之间用空格分开
+        wd.loadUserDict(Paths.get(ResourceUtils.getFile("classpath:user-dict.txt").getAbsolutePath()));
+        process = segmenter.process(str, JiebaSegmenter.SegMode.SEARCH);
         for (SegToken st : process) {
             String word = st.word;
             System.out.println(word);
