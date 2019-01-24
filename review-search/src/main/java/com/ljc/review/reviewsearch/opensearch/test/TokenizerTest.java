4d42e157bd593c2f223f4d18c3655de7478f618b
lijiangc <lijiangcheng@git.com>
2019-01-24 15:26:40 +0800

    添加es依赖

--- review-search/src/main/java/com/ljc/review/reviewsearch/opensearch/test/TokenizerTest.java
+++ review-search/src/main/java/com/ljc/review/reviewsearch/opensearch/test/TokenizerTest.java
@@ -24,20 +24,21 @@ public class TokenizerTest {
     public void testJieBa() throws IOException {
         JiebaSegmenter segmenter = new JiebaSegmenter();
         WordDictionary wd = WordDictionary.getInstance();
-        String str = "300~800W、13mm";
+        String str = "星工（XINGGONG）口罩 防尘防雾霾PM2.5活性炭呼吸阀折叠耳戴式透气 XG8831CV5只装";
         //str = str.replaceAll(" ", "");
         List<SegToken> process = segmenter.process(str, JiebaSegmenter.SegMode.SEARCH);
         for (SegToken st : process) {
             String word = st.word;
             System.out.println(word);
         }
+
         //加载自定义词典 PS：jeiba自定义词典规则：一行一个词，分别为：词条/词频/词性(可省略)，之间用空格分开
-        wd.loadUserDict(Paths.get(ResourceUtils.getFile("classpath:user-dict.txt").getAbsolutePath()));
+        /*wd.loadUserDict(Paths.get(ResourceUtils.getFile("classpath:user-dict.txt").getAbsolutePath()));
         process = segmenter.process(str, JiebaSegmenter.SegMode.SEARCH);
         for (SegToken st : process) {
             String word = st.word;
             System.out.println(word);
-        }
+        }*/
     }
 
     @Test
@@ -50,4 +51,33 @@ public class TokenizerTest {
         }
     }
 
+    @Test
+    public void testZhengZ() {
+        String productName = "按钮盒button boxes 施耐德 XALB03C";
+        //处理别名
+        productName = productName.trim().replaceAll("、", "/").replaceAll("[\\s\\u00A0]+", "/")
+                .replaceAll(",", "/").replaceAll("，", "/");
+        System.out.println(productName);
+    }
+
+    @Test
+    public void testStringBuilder() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("hello");
+        sb.insert(0, "world");
+        System.out.println(sb.toString());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
