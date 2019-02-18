3529af9630b55d552ed33d93805d24cce9b1f78e
lijiangc <lijiangcheng@git.com>
2019-02-18 18:42:37 +0800

    框架

---
+++ review-search/src/main/java/com/ljc/review/reviewsearch/elasticsearch/config/ElasticSearchConfig.java
@@ -0,0 +1,10 @@
+package com.ljc.review.reviewsearch.elasticsearch.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+
+
+
+}
