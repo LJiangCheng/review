1a1426df888ee31706cc97a4e5aeffb4f5e8263c
lijc <418734183@qq.com>
2019-03-10 21:17:39 +0800

    auth项目依赖和MainClass配置

---
+++ review-auth/src/main/java/com/ljc/review/auth/ReviewAuthApplication.java
@@ -0,0 +1,12 @@
+package com.ljc.review.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAuthApplication.class, args);
+	}
+}
