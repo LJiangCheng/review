a97e8a17284af50f1da6ee440e4858d00f8e6840
lijiangc <lijiangcheng@git.com>
2019-05-09 20:08:07 +0800

    加入swagger

--- review-search/src/main/java/com/ljc/review/search/controller/ProductDataController.java
+++ review-search/src/main/java/com/ljc/review/search/controller/ProductDataController.java
@@ -2,6 +2,7 @@ package com.ljc.review.search.controller;
 
 import com.ljc.review.search.entity.common.BaseResult;
 import com.ljc.review.search.service.spec.ProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class ProductDataController {
     @Autowired
     private ProductService productService;
 
+    @ApiOperation(value = "重建商品索引", notes = "@Author ljc")
     @PostMapping("pushAllProductToES")
     public BaseResult pushAllProductToES() {
         productService.pushAllProductToES();
