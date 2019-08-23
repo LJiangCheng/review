988071057638379040784341c18a63e5f1acb2c3
lijiangc <lijiangcheng@git.com>
2019-08-23 17:50:11 +0800

    中心水印

--- review-basic/src/main/java/com/ljc/review/common/image/TestImage.java
+++ review-basic/src/main/java/com/ljc/review/common/image/TestImage.java
@@ -16,12 +16,13 @@ public class TestImage {
     @Test
     public void testWaterMarker() {
         //水印图
-        File waterMarkFile = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\water.png");
+        File logo = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\water.png");
         //原图
-        File srcFile = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\400.jpg");
+        File source = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\400.jpg");
         //目标文件
-        File destFile = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\500.jpg");
-        ImageProcessor.imageAddWaterMark(srcFile, waterMarkFile, destFile, 1, 0.3F, 100);
+        File destFile = new File("C:\\Users\\toolmall\\Desktop\\watermaker\\4000.jpg");
+        ImageProcessor.fullLogo(source, logo, destFile, 1, 0.1F, 100);
+        //ImageProcessor.centerLogo(source, logo, destFile, 1, 0.1F, 100);
     }
 
     /**
--- review-basic/src/main/java/com/ljc/review/common/image/watermark/ImageProcessor.java
+++ review-basic/src/main/java/com/ljc/review/common/image/watermark/ImageProcessor.java
@@ -9,13 +9,42 @@ import java.io.File;
 public class ImageProcessor {
 
     /**
-     * 为整张图片打满水印
+     * 中心水印
      * @param srcFile  原始图片文件
      * @param waterMarkFile 水印图片文件
      * @param dstFile 添加水印后的文件
      * @param sigma 西格玛值
      */
-    public static void imageAddWaterMark(File srcFile, File waterMarkFile, File dstFile, double sigma, float weight, int Quality) {
+    public static void centerLogo(File srcFile, File waterMarkFile, File dstFile, double sigma, float weight, int Quality) {
+        ImageUtil imageUtil = new ImageUtil();
+        // 原始图片
+        ImageData srcImage = imageUtil.readImage(srcFile);
+        // 水印图片
+        ImageData waterMarkImage = imageUtil.readImage(waterMarkFile);
+        // 计算坐标地址
+        int srcWidth = srcImage.getWidth(), srcHeight = srcImage.getHeight();
+        int logoWidth = waterMarkImage.getWidth(), logoHeight = waterMarkImage.getHeight();
+        int x = srcWidth / 2 - logoWidth / 2;
+        int y = srcHeight / 2 - logoHeight / 2;
+        if(x < 0) x = 0;
+        if(y < 0) y = 0;
+        //打水印
+        srcImage = LogoInserter.insertLogo(srcImage, waterMarkImage, x, y);
+        // 图片锐化
+        Sharpen sh = new Sharpen();
+        ImageData tempImg = sh.ImageSharpen(srcImage, sigma, weight);
+        // 输出图片
+        imageUtil.writeImage(tempImg, dstFile, Quality);
+    }
+
+    /**
+     * 全图水印
+     * @param srcFile  原始图片文件
+     * @param waterMarkFile 水印图片文件
+     * @param dstFile 添加水印后的文件
+     * @param sigma 西格玛值
+     */
+    public static void fullLogo(File srcFile, File waterMarkFile, File dstFile, double sigma, float weight, int Quality) {
         ImageUtil imageUtil = new ImageUtil();
         // 原始图片
         ImageData srcImage = imageUtil.readImage(srcFile);
@@ -23,9 +52,9 @@ public class ImageProcessor {
         ImageData waterMarkImage = imageUtil.readImage(waterMarkFile);
         // 全图打上水印
         int srcWidth = srcImage.getWidth(), srcHeight = srcImage.getHeight();
-        int waterMarkWidth = waterMarkImage.getWidth(), waterMarkHeight = waterMarkImage.getHeight();
-        for (int y=0; y<srcHeight; y+=waterMarkHeight) {
-            for (int x=0; x<srcWidth; x+=waterMarkWidth) {
+        int logoWidth = waterMarkImage.getWidth(), logoHeight = waterMarkImage.getHeight();
+        for (int y=0; y<srcHeight; y+=logoHeight) {
+            for (int x=0; x<srcWidth; x+=logoWidth) {
                 srcImage = LogoInserter.insertLogo(srcImage, waterMarkImage, x, y);
             }
         }
