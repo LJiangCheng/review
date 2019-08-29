a5d038d864b873018d3cfd5e9e216f84b78512c5
ljiangc <418734183@qq.com>
2019-08-29 23:07:32 +0800

    授权可运行

--- review-web/src/main/java/com/ljc/review/web/config/OAuth2Config.java
+++ review-web/src/main/java/com/ljc/review/web/config/OAuth2Config.java
@@ -3,9 +3,12 @@ package com.ljc.review.web.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -14,6 +17,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
@@ -62,29 +66,39 @@ public class OAuth2Config {
         AuthenticationManager authenticationManager;
         @Autowired
         RedisConnectionFactory redisConnectionFactory;
+        @Autowired
+        UserDetailsService userDetailsService;
+        @Autowired
+        private BCryptPasswordEncoder passwordEncoder;
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             //配置两个客户端,一个用于password认证一个用于client认证
             clients.inMemory().withClient("client_1")
                     .resourceIds(DEMO_RESOURCE_ID)
-                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .authorizedGrantTypes("client_credentials")
                     .scopes("select")
-                    .authorities("client")
-                    .secret("123456")
+                    .authorities("oauth2")
+                    .secret(passwordEncoder.encode("123456"))
                     .and().withClient("client_2")
                     .resourceIds(DEMO_RESOURCE_ID)
                     .authorizedGrantTypes("password", "refresh_token")
                     .scopes("select")
-                    .authorities("client")
-                    .secret("123456");
+                    .authorities("oauth2")
+                    .secret(passwordEncoder.encode("123456"));
         }
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
             endpoints
                     .tokenStore(new RedisTokenStore(redisConnectionFactory))
-                    .authenticationManager(authenticationManager);
+                    .tokenStore(new InMemoryTokenStore())
+                    .authenticationManager(authenticationManager)
+                    .userDetailsService(userDetailsService)
+                    // 2018-4-3 增加配置，允许 GET、POST 请求获取 token，即访问端点：oauth/token
+                    .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+
+            endpoints.reuseRefreshTokens(true);
         }
 
         @Override
---
+++ review-web/src/main/java/com/ljc/review/web/config/SpringSecurityAdapter.java
@@ -0,0 +1,57 @@
+package com.ljc.review.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password(getBCryptPasswordEncoder().encode("123456")).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(getBCryptPasswordEncoder().encode("123456")).authorities("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //inMemoryAuthentication 从内存中获取
+        auth.userDetailsService(userDetailsService()).passwordEncoder(getBCryptPasswordEncoder());
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .requestMatchers().anyRequest()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/*").permitAll();
+        // @formatter:on
+    }
+
+}
