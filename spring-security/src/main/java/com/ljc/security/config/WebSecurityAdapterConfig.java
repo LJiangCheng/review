3ae3480ca0a89a5aa1db0c255cea7651607e3445
lijiangc <lijiangcheng@git.com>
2019-09-06 17:45:23 +0800

    自定义登录及处理逻辑

--- spring-security/src/main/java/com/ljc/security/config/WebSecurityAdapterConfig.java
+++ spring-security/src/main/java/com/ljc/security/config/WebSecurityAdapterConfig.java
@@ -1,5 +1,6 @@
 package com.ljc.security.config;
 
+import com.ljc.security.filter.CustomAuthenticationFilter;
 import com.ljc.security.handler.LoginFailureHandler;
 import com.ljc.security.handler.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +30,8 @@ public class WebSecurityAdapterConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()                         //支持表单登录，WebSecurityConfigurerAdapter默认应用
                 .loginPage("/login").permitAll()     //指定登录页面的路径，以及允许所有请求访问该路径 PS：如果不指定则使用自带的默认页
-                .successHandler(loginSuccessHandler())   //指定登录成功、失败之后的处理器
-                .failureHandler(loginFailureHandler())
+                //.successHandler(loginSuccessHandler())   //指定登录成功、失败之后的处理器(对自定义过滤器不管用)
+                //.failureHandler(loginFailureHandler())
                 .and().httpBasic()
                 .and().csrf().disable();
 
@@ -38,18 +41,28 @@ public class WebSecurityAdapterConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/index")
                 .invalidateHttpSession(true)
-                .deleteCookies("cookieName");
+                .deleteCookies("JSESSIONID");
                 /*
                 .logoutSuccessHandler(logoutSuccessHandler) //指定一个自定义LogoutSuccessHandler，如果指定了此项，则忽略logoutSuccessUrl()
                 .addLogoutHandler(logoutHandler)  //添加参与注销处理的LogoutHandler对象。默认情况下，SecurityContextLogoutHandler被添加为最后一个LogoutHandler
                 .and()...*/
 
-        //自定义一系列登录过滤器在默认的过滤器之前用于不同用途的登录操作
-        /*http.addFilterBefore(this.memberUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(this.qqAuthenticationFilter(), MemberUsernamePasswordAuthenticationFilter.class);
+        //自定义登录过滤器
+        http.addFilterBefore(this.customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        //第三方登录过滤器
+        /*http.addFilterBefore(this.qqAuthenticationFilter(), MemberUsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(this.wechatAuthenticationFilter(), MemberUsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(this.registerSuccessFilter(), MemberUsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(this.fanliAuthenticationFilter(), MemberUsernamePasswordAuthenticationFilter.class);*/
+    }
+
+    @Bean
+    public Filter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter authenticationFilter = new CustomAuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(super.authenticationManagerBean());
+        authenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        authenticationFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return authenticationFilter;
     }
 
     @Bean
--- spring-security/src/main/java/com/ljc/security/handler/LoginFailureHandler.java
+++ spring-security/src/main/java/com/ljc/security/handler/LoginFailureHandler.java
@@ -3,18 +3,20 @@ package com.ljc.security.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class LoginFailureHandler implements AuthenticationFailureHandler {
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginFailureHandler.class);
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
         LOGGER.info("登陆失败，进行了一些操作...");
+        //重定向等系统操作
+        super.onAuthenticationFailure(httpServletRequest, httpServletResponse, e);
     }
 }
