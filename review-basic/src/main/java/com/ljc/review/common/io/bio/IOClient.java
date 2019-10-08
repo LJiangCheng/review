fde5ec9740273c07aef512916011a3464a5e6fa0
lijiangc <lijiangcheng@git.com>
2019-10-08 14:50:07 +0800

    BIO

---
+++ review-basic/src/main/java/com/ljc/review/common/io/bio/IOClient.java
@@ -0,0 +1,32 @@
+package com.ljc.review.common.io.bio;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * 模拟io客户端
+ */
+public class IOClient {
+
+    public static void main(String[] args) {
+        //并发模拟
+        for(int i=1;i<6;i++) {
+            int j = i;
+            new Thread(() -> {
+                try {
+                    Socket socket = new Socket("127.0.0.1", 3333);
+                    int n = 0;
+                    while (n++ < 2) {
+                        try {
+                            socket.getOutputStream().write(("Thread" + j + ": hello world").getBytes());
+                            Thread.sleep(2000);
+                        } catch (Exception e) {
+                        }
+                    }
+                } catch (IOException e) {
+                }
+            }).start();
+        }
+    }
+
+}
---
+++ review-basic/src/main/java/com/ljc/review/common/io/bio/IOServer.java
@@ -0,0 +1,46 @@
+package com.ljc.review.common.io.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * IO服务端：使用多线程模拟异步IO
+ *
+ * 1.单线程同时只能处理一个连接请求
+ * 2.基于多线程(使用线程池)可以获得类似异步IO的效果，但如果并发数过多则无法处理，要么请求阻塞要么由于线程数过多导致系统崩溃
+ * 3.在活动连接数不是特别高(小于单机1000)的情况下，这种模型是比较不错的，可以让每一个连接专注于自己的I/O并且编程模型简单，
+ * 也不用过多考虑系统的过载、限流等问题。线程池本身就是一个天然的漏斗，可以缓冲一些系统处理不了的连接或请求。但是，当面对十万
+ * 甚至百万级连接的时候，传统的BIO模型是无能为力的
+ */
+public class IOServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3333);
+        // 接收到客户端连接请求之后为每个客户端创建一个新的线程进行链路处理
+        new Thread(() -> {
+            while (true) {
+                try {
+                    // 阻塞方法获取新的连接
+                    Socket socket = serverSocket.accept();
+                    // 每一个新的连接都创建一个线程，负责读取数据
+                    new Thread(() -> {
+                        try {
+                            int len;
+                            byte[] data = new byte[1024];
+                            InputStream inputStream = socket.getInputStream();
+                            // 按字节流方式读取数据
+                            while ((len = inputStream.read(data)) != -1) {
+                                System.out.println(new String(data, 0, len));
+                            }
+                        } catch (IOException e) {
+                        }
+                    }).start();
+
+                } catch (IOException e) {
+                }
+            }
+        }).start();
+    }
+}
