b9e408268b7f7ffa335b9489d37c651728dca2fd
lijiangc <lijiangcheng@git.com>
2020-04-26 15:53:24 +0800

    快排优化和速度测试

--- review-alg/src/main/java/com/ljc/alg/sort/InsertionSort.java
+++ review-alg/src/main/java/com/ljc/alg/sort/InsertionSort.java
@@ -10,14 +10,21 @@ public class InsertionSort extends AbstractSort {
      */
     @Override
     public void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * 可指定要排序的子序列
+     */
+    public void sort(int[] arr, int left, int right) {
+        int j;
         //从数组的第二个元素开始往前寻找插入点
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = left + 1; i < right + 1; i++) {
             //记录第i个元素
             int temp = arr[i];
             //将元素不断向前比较，如果temp小于前面的元素，则将前面的元素后移，temp临时占据该元素的位置。
             //如果temp不小于前面的元素，则说明已经找到正确的位置，temp可确实占据当前位置
-            int j;
-            for (j = i; j > 0 && temp < arr[j - 1]; j--) {
+            for (j = i; j > left && temp < arr[j - 1]; j--) {
                 //后移一位
                 arr[j] = arr[j - 1];
             }
@@ -26,3 +33,16 @@ public class InsertionSort extends AbstractSort {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- review-alg/src/main/java/com/ljc/alg/sort/QuickSort.java
+++ review-alg/src/main/java/com/ljc/alg/sort/QuickSort.java
@@ -8,6 +8,8 @@ import com.ljc.alg.sort.inter.AbstractSort;
  */
 public class QuickSort extends AbstractSort {
 
+    private InsertionSort insertionSort = new InsertionSort();
+
     /**
      * 快排驱动程序
      * 选取基准元 --暂缺，直接使用最右侧元素作为基准元
@@ -15,12 +17,12 @@ public class QuickSort extends AbstractSort {
      */
     @Override
     public void sort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
+        moreQuickSort(arr, 0, arr.length - 1);
     }
 
     /**
      * 快排执行程序
-     *
+     * 估时：1亿-15S 2亿-31S
      * @param arr   数组
      * @param left  序列左界
      * @param right 序列右界
@@ -48,6 +50,7 @@ public class QuickSort extends AbstractSort {
         //需要最后判断一次基准元和i所在元素的大小
         //解释：正常来说，arr[i]<n时，i一定会向前推进。
         //但如果递归到最后arr中只有两个元素的时候，ij初始化的时候就已经相等，根本不会进入循环，此时就需要进行一次如下的比较以防止错误的交换
+        //后续优化之后，如果数组长度小于12则使用插入排序，那么此处判断就是不必要的了
         if (arr[i] < n) {
             i++;
         }
@@ -56,6 +59,37 @@ public class QuickSort extends AbstractSort {
         //二分法，递归调用
         quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
+    }
+
+    /**
+     * 快排执行程序优化
+     * 估时：1亿-15S 2亿~31S  基本上和上面的只有毫秒级的差距，似乎没有明显的速度提升？
+     *
+     * @param arr   数组
+     * @param left  序列左界
+     * @param right 序列右界
+     */
+    private void moreQuickSort(int[] arr, int left, int right) {
+        //递归终止条件
+        if (right - left > 10) {
+            int n = arr[right];
+            int i = left, j = right - 1;
+            while (i < j) {
+                //换种写法[免得出现重复提示]
+                for (; arr[j] >= n && j > left; ) j--;
+                for (; arr[i] <= n && i < right; ) i++;
+                if (i < j) {
+                    swap(arr, i, j);
+                }
+            }
+            //将基准元放到合适的位置
+            swap(arr, right, i);
+            //二分法，递归调用
+            moreQuickSort(arr, left, i - 1);
+            moreQuickSort(arr, i + 1, right);
+        } else {
+            insertionSort.sort(arr, left, right);
+        }
     }
 
 }
