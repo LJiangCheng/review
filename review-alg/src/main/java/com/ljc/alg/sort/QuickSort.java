756bb045ad145d73cf50f2d0c3e0f66114dbbf28
lijiangc <lijiangcheng@git.com>
2020-04-26 13:11:21 +0800

    快排 --可执行

--- review-alg/src/main/java/com/ljc/alg/sort/QuickSort.java
+++ review-alg/src/main/java/com/ljc/alg/sort/QuickSort.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] arr = {1, 5, 2, 4, 6, 1, 3, 5, 1, 4, 234, 23, 423, 423, 4, 5, 214, 4545, 467, 788, 234, 123, 45, 12231, 4564, 13, 4, 1231, 12};
+        int[] arr = {1, 5, 2, 4, 6, 1, 3, 5, 1, 4, 234, 1231, 11, 9, 23, 423, 423, 4, 5, 214, 4545, 467, 788, 234, 123, 45, 12231, 4564, 13, 4, 1231, 12};
         //int[] arr = {2, 5, 1, 8, 6};
         quickSort(arr);
         System.out.println(Arrays.toString(arr));
@@ -33,49 +33,34 @@ public class QuickSort {
      * @param right 序列右界
      */
     private static void quickSort(int[] arr, int left, int right) {
-        //递归终止
-        /*if (right - left < 1) {
-            return;
-        }*/
+        //递归终止 改进：当数组元素小于10个时使用插入排序完成剩下的工作
         if (left >= right) {
             return;
         }
-        //直接取最右边的元素为中间数
+        //直接取最右边的元素为基准元 改进：使用三数中值
         int n = arr[right];
+        //i从最左，j从基准元前一个开始移动
         int i = left, j = right - 1;
         //遍历交换
-        while (i != j) {
-            //从右往左，直到小于N或i-j交错时停止；保证右侧全是大于等于N的值
-            while (arr[j] >= n && i < j) j--;
-            //从左往右，直到大于N或i-j交错时停止；保证左侧全是小于等于N的值
-            while (arr[i] <= n && i < j) i++;
-            //如果
+        while (i < j) {
+            //从右往左，直到小于N时停止；保证右侧全是大于等于N的值
+            while (arr[j] >= n && j > left) j--;
+            //从左往右，直到大于N时停止；保证左侧全是小于等于N的值
+            while (arr[i] <= n && i < right) i++;
+            //如果i,j没有交错，则交换所在位置的值
             if (i < j) {
                 swap(arr, i, j);
             }
         }
-        /*for (; i <= right - 1; i++) {
-            //从左向右遍历，如果遇到大于中间数，等待右侧游标
-            if (arr[i] >= n) {
-                for (; j > left; j--) {
-                    //从右向左遍历，如果遇到小于中间数，交换i、j所在数组元素
-                    if (arr[j] <= n) {
-                        swap(arr, i, j);
-                        break;
-                    }
-                }
-            }
-        }*/
-        //将中间值放到合适的位置
-        if (arr[j] >= arr[right]) {
-            swap(arr, right, j);
-        } else if (j + 1 < right && arr[j + 1] > arr[right]) {
-            swap(arr, right, j + 1);
-            j++;
+        //需要最后判断一次基准元和i所在元素的大小：i所在元素可能会小于n，但是因为ij已经交错而没有可交换对象？？？
+        if (arr[i] < n) {
+            i++;
         }
+        //将基准元放到合适的位置
+        swap(arr, right, i);
         //二分法，递归调用
-        quickSort(arr, left, j - 1);
-        quickSort(arr, j + 1, right);
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
     }
 
     private static void swap(int[] arr, int i, int j) {
