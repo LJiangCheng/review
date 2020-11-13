81903bb15d3c2ac9fc345cd8998ca1d62bea4a74
lijiangc <lijiangcheng@git.com>
2020-11-13 17:23:27 +0800

    interrupt和park

--- review-basic/src/main/java/com/ljc/review/common/test/ParkTest.java
+++ review-basic/src/main/java/com/ljc/review/common/test/ParkTest.java
@@ -10,13 +10,13 @@ public class ParkTest {
 
     public static void testLockSupport() throws InterruptedException {
         Thread thread = new Thread(new ThreadPark(), "park");
-        thread.start();
-        Thread.sleep(1000);
-        thread.interrupt();
-        System.out.println("是否中断1:" + thread.isInterrupted());
+        thread.start(); //park
+        Thread.sleep(1000); //主线程等待
+        thread.interrupt(); //设置thread中断状态，中断thread  这将导致thread从park中恢复
+        System.out.println("是否中断1:" + thread.isInterrupted() + " " + thread.getState()); //打印中断状态 可能是true or false
         Thread thread2 = new Thread(new ThreadUnPark(thread), "unPark");
-        thread2.start();
-        System.out.println("是否中断2:" + thread.isInterrupted());
+        thread2.start(); //sleep 5000 and unpark thread
+        System.out.println("是否中断2:" + thread.isInterrupted()); //打印中断状态 只会是false
         Thread.sleep(6000);
     }
 
@@ -24,7 +24,11 @@ public class ParkTest {
         @Override
         public void run() {
             System.out.println("ThreadPark开始");
-            LockSupport.park(this);//阻塞当前线程
+            System.out.println("当前阻塞的线程：" + Thread.currentThread().getName());
+            LockSupport.park(Thread.currentThread());//阻塞当前线程
+            /*for (int i = 0; i < 1000000000; i++) {
+                int y = i + 1;
+            }*/
             System.out.println("ThreadPark结束");
         }
     }
@@ -44,7 +48,9 @@ public class ParkTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            LockSupport.park(thread);//阻塞线程
+            System.out.println("当前线程：" + Thread.currentThread().getName());
+            System.out.println("将被唤醒的线程：" + thread.getName());
+            LockSupport.unpark(thread);//唤醒被阻塞的线程
             System.out.println("ThreadUnPark结束");
         }
     }
