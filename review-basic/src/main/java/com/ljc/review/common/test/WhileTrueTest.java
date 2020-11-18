537fe35cf2bfbe109ff2ae0e571ff2274ac32487
lijiangc <lijiangcheng@git.com>
2020-11-18 13:24:47 +0800

    WhileTrueTest

---
+++ review-basic/src/main/java/com/ljc/review/common/test/WhileTrueTest.java
@@ -0,0 +1,12 @@
+package com.ljc.review.common.test;
+
+public class WhileTrueTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println("银鞍照白马");
+            Thread.sleep(30);
+        }
+    }
+
+}
